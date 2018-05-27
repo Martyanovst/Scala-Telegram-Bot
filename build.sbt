4c43687e58bafff0e76f66b9039460e70866c914
Ozimandias <pavelevivan@mail.ru>
2018-05-27 17:48:30 +0500

    Context Command on question changed

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.4"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0"
-
+libraryDependencies += "info.mukel" %% "telegrambot4s" % "3.0.14"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
         
--- src/main/scala/main/scala/Main.scala
+++ src/main/scala/main/scala/Main.scala
@@ -1,8 +1,18 @@
 package main.scala
-
-
+import info.mukel.telegrambot4s.api.TelegramBot
+import info.mukel.telegrambot4s.api.Polling
+import info.mukel.telegrambot4s.api.declarative.{Callbacks,Commands}
 import scala.io.Source
 
+
+class MyBot extends TelegramBot with Polling with Commands{
+  override def token = "499150688:AAHcHDrlzM_MGLEm5s9p5JrsdQy2stqd3TY"
+  onCommand("help"){
+  }
+  onCommand("delete"){}
+  onCommand("")
+  onCommand("begin"){}
+}
 object Main extends App {
   val parser = new CommandParser()
   Source.fromFile("MyTimeTest.txt").getLines
