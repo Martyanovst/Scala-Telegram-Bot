728f92a2faee5f13b931468ff3b0ee640a1436ae
Ozimandias <pavelevivan@mail.ru>
2018-05-27 17:49:47 +0500

    Added class MyBot

--- src/main/scala/main/scala/Main.scala
+++ src/main/scala/main/scala/Main.scala
@@ -5,14 +5,14 @@ import info.mukel.telegrambot4s.api.declarative.{Callbacks,Commands}
 import scala.io.Source
 
 
-class MyBot extends TelegramBot with Polling with Commands{
-  override def token = "499150688:AAHcHDrlzM_MGLEm5s9p5JrsdQy2stqd3TY"
-  onCommand("help"){
-  }
-  onCommand("delete"){}
-  onCommand("")
-  onCommand("begin"){}
-}
+//class MyBot extends TelegramBot with Polling with Commands{
+//  override def token = "499150688:AAHcHDrlzM_MGLEm5s9p5JrsdQy2stqd3TY"
+//  onCommand("help"){
+//  }
+//  onCommand("delete"){}
+//  onCommand("")
+//  onCommand("begin"){}
+//}
 object Main extends App {
   val parser = new CommandParser()
   Source.fromFile("MyTimeTest.txt").getLines
